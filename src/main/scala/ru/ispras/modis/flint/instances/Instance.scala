a3ef051d1d3ef702d4a44d5ab6ef17e5537ad20c
valerij <acopich@gmail.com>
2013-07-26 22:06:21 +0400

    comment

--- src/main/scala/ru/ispras/modis/flint/instances/Instance.scala
+++ src/main/scala/ru/ispras/modis/flint/instances/Instance.scala
@@ -1,10 +1,16 @@
 package ru.ispras.modis.flint.instances
 
+
 /**
  * Created with IntelliJ IDEA.
  * User: valerij
  * Date: 7/22/13
  * Time: 5:08 PM
+ *
+ * This class represents a data instance
+ *
+ * Do not forget to implement InstanceBuilder trait for every
+ * inheritor and to implement the corresponding implicit method in InstanceBuilder object
  */
 class Instance(private val featuresToWeights: IndexedSeq[Feature]) extends IndexedSeq[Feature] with Serializable {
     override def iterator: Iterator[Feature] = featuresToWeights.iterator
