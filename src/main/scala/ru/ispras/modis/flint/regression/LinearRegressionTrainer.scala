7599055cb0eb3d4c8858a3e4b106211476359efd
Mikhail Shalaev <mikesaylars@gmail.com>
2013-11-13 17:23:14 +0400

    update git && first try

--- src/main/scala/ru/ispras/modis/flint/regression/LinearRegressionTrainer.scala
+++ src/main/scala/ru/ispras/modis/flint/regression/LinearRegressionTrainer.scala
@@ -15,13 +15,16 @@ class LinearRegressionTrainer(l2regularization: Double) extends RegressionTraine
     def sqr(x:Double):Double = {x * x}
 
     def apply(data: RDD[LabelledInstance[Double]]): LinearRegressionModel = {
+
         val data1 = data.cache()
         val randarr = new Array[Double](data1.first().length)
         for (i <- 0 to randarr.length - 1)
             randarr(i) = java.lang.Math.random()
         var tmp = new LinearRegressionModel(randarr)
+
         var err = data1.map(p => sqr(p.label - tmp.predicts(p))).reduce(_ + _)
         var preverr = 0.0
+
         var i = 0
         while (math.abs(err - preverr) > 0.01 && i < 100) {
 
@@ -39,15 +42,18 @@ class LinearRegressionTrainer(l2regularization: Double) extends RegressionTraine
             preverr = err
             val delta = 0.5
             var sumgrad = 0.0
+
             for (j <- 0 to gradient.length - 1){
                 sumgrad += sqr(gradient(j))
             }
+
             var tmp1:LinearRegressionModel = tmp.copy()
             do {
                 alpha = alpha * delta
                 tmp1 = tmp + (gradient + tmp * (-1 * l2regularization)) * alpha
                 err = data1.map(p => sqr(p.label - tmp1.predicts(p))).reduce(_ + _)
             } while (err - preverr > 0.01 * alpha * sumgrad)
+
             tmp = tmp1.copy()
             i += 1
         }
