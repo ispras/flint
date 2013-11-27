9e2ae09167f7662fc1c3f842a895fb196eb9f0c7
linafru <linafruscianta@gmail.com>
2013-11-27 18:19:32 +0400

    first estimation

--- src/main/scala/ru/ispras/modis/flint/classifiers/NaiveBayes/BayesEstimation.scala
+++ src/main/scala/ru/ispras/modis/flint/classifiers/NaiveBayes/BayesEstimation.scala
@@ -2,6 +2,8 @@ package flint.src.main.scala.ru.ispras.modis.flint.classifiers.NaiveBayes
 
 import ru.ispras.modis.flint.classifiers.{DensityEstimator, DensityEstimation}
 import org.apache.spark.rdd.RDD
+import ru.ispras.modis.flint.instances.Instance
+import scala.math.log
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,8 +12,16 @@ import org.apache.spark.rdd.RDD
  * Time: 13:39
  * To change this template use File | Settings | File Templates.
  */
-class BayesEstimation[LabelType](sample: RDD[(LabelType,Int,Int)]) extends DensityEstimation[LabelType] {
+class BayesEstimation[LabelType](features: Map[(LabelType,Int,Double),Long]) extends DensityEstimation[LabelType] {
 
-    override def apply(label: LabelType,featureId: Int,value: Double) : Double = value
+    override def apply(label: LabelType,featureId: Int, weight: Double) : Double = {
+
+      val featureSum = features.map(_._2).sum
+
+      val featureprob = features.map{case (feature, weight) => log(weight.toDouble/featureSum)}
+
+      val prioritySum = featureprob.foldLeft(Double)((sum,element: Double) => sum + element)
+
+    }
 
 }
