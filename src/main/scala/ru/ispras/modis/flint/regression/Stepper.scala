7ef9006b8142d62657e94abb2a6fc47419f177f5
Mikhail Shalaev <mikesaylars@gmail.com>
2013-12-03 16:02:44 +0400

    add scalala

---
+++ src/main/scala/ru/ispras/modis/flint/regression/Stepper.scala
@@ -0,0 +1,41 @@
+package ru.ispras.modis.flint.regression
+
+import ru.ispras.modis.flint.instances.LabelledInstance
+import org.apache.spark.rdd.RDD
+import scalala.tensor.dense.DenseVector
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: saylars
+ * Date: 15.11.13
+ * Time: 16:08
+ */
+trait Stepper {
+
+    def nextStep(data: RDD[LabelledInstance[Double]],
+                 model : LinearRegressionModel,
+                 grad : DenseVector[Double],
+                 l2regularization : Double,
+                 oldSquareErr: Double) = {
+
+        var alpha = 1.0
+        val delta = 0.5
+
+        val normGrad = (grad :* grad).sum
+
+
+        var shiftedModel = new LinearRegressionModel(DenseVector.zeros[Double](model.length))
+
+        var newSquareErr = 0.0
+
+        do {
+            alpha = alpha * delta
+            shiftedModel = new LinearRegressionModel(model.data :+ ((grad :- (model.data  * l2regularization)) * alpha))
+            newSquareErr = data.map(p => {
+                val x = p.label - shiftedModel.predicts(p)
+                x * x
+            }).reduce(_ + _)
+        } while (newSquareErr - oldSquareErr > 0.001 * alpha * normGrad)
+        (newSquareErr, shiftedModel)
+    }
+}
