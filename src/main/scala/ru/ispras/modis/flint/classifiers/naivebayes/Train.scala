d782a2d67b152477e40f3ec1033c123e40e53d48
linafru <linafruscianta@gmail.com>
2013-12-04 15:01:37 +0400

    train on heart data

--- src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/Train.scala
+++ src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/Train.scala
@@ -4,10 +4,11 @@ import spark.SparkContext
 import scala.io.Source
 import ru.ispras.modis.flint.instances.{BinaryFeature, WeightedFeature, LabelledInstance,Instance}
 import java.io.File
+import ru.ispras.modis.flint.crossvalidation.ClassifierCrossValidator
 
 object Train extends App {
 
-  val sc = new SparkContext("local[2]","")
+  val sc = new SparkContext("local[1]","")
 
   val spect_dataset = Source.fromFile(new File("/home/lena/heart_dataset/SPECT.train")).getLines()
 
@@ -31,11 +32,13 @@ object Train extends App {
         new Instance(Features.zipWithIndex.map{case (weight, id) => new WeightedFeature(id,weight)}.toIndexedSeq)}).toSeq)
 
 
-      val train = new BayesEstimator[Int].apply(data)
+      val train = new BayesEstimator[Int]
 
-      val trainClassifier = new BayesClassifierTrainer[Int](data, train).apply(data)
+      val trainClassifier = new BayesClassifierTrainer[Int](data, train).apply(data) //return classifier
 
-      val testInstance = datatest.map(instance => trainClassifier.apply(instance))
+      val classifier = datatest.map(instance => trainClassifier.apply(instance))
+      /*printf("the result", classifier.map(res => println(res)))
+      new ClassifierCrossValidator[Int]().apply(new BayesClassifierTrainer[Int](data,train),data)   */
 
 
 }
