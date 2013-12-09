e497d32c3d02c10ce9da13ff9c29121a2ae0c5ed
linafru <linafruscianta@gmail.com>
2013-12-09 18:55:46 +0400

    train on heart data

--- src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/BayesEstimator.scala
+++ src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/BayesEstimator.scala
@@ -12,10 +12,11 @@ class BayesEstimator[LabelType: ClassManifest] extends DensityEstimator[LabelTyp
 
     val labelCount = data.map(instance => instance.label).countByValue()
 
-    val labelIdWeight :Map[(LabelType,Int,Double), Long] = data.flatMap(instance =>
-      instance.map(feature => (instance.label, feature.featureId, feature.featureWeight))).countByValue().toMap
-     /*println(labelIdWeight)
-    println(labelIdWeight.size)*/
+    val labelIdWeight: Map[(LabelType,Int,Double),Long] = data.flatMap(instance =>
+      instance.map(feature =>  (instance.label, feature.featureId, feature.featureWeight))).countByValue().toMap
+
+
+
      val eps = 0.000001
 
      val addEps = labelIdWeight.map{case ((label,id,weight),prob) => ((label,id, weight + eps),prob)}
@@ -24,10 +25,10 @@ class BayesEstimator[LabelType: ClassManifest] extends DensityEstimator[LabelTyp
 
         if(abs(weight - 1.0) < eps) {((label,id,true),prob)} else {((label,id,false),prob)}}
 
+    val featureLogProb  :Map[(LabelType,Int,Boolean), Double] = labelIdFeatureBoolean.map{case ((label, featureId, weight), value) =>
 
-      println(labelIdFeatureBoolean)
-      println(labelIdWeight)
-    val featureLogProb  :Map[(LabelType,Int,Boolean), Double] = labelIdFeatureBoolean.map{case ((label, featureId, weight), value) => ((label, featureId,weight),log(value.toDouble/labelCount(label)))}
+      if (value != 0) {((label, featureId,weight),log(value.toDouble/labelCount(label)))} else {((label, featureId,weight),log(1.0/labelCount(label)))}}
+
 
     new BinaryEstimation[LabelType](featureLogProb)
  }
--- src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/Train.scala
+++ src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/Train.scala
@@ -13,35 +13,32 @@ object Train extends App {
   val sc = new SparkContext("local[1]","")
 
   val spect_dataset = Source.fromFile(new File("/home/lena/heart_dataset/SPECT.train")).getLines()
-
+       //spect_dataset.foreach(println)
   val data = sc.parallelize(spect_dataset.map(line => {
 
-     val labelFeatures = line.split(",").map(_.toInt).toList
+         val label :: features = line.split(",").map(_.toInt).toList
 
-     val label = labelFeatures.head
-
-     val features = labelFeatures.tail.map(_.toDouble)
-
-     new LabelledInstance[Int](features.zipWithIndex.map{case (weight, id) => new WeightedFeature(id,weight)}.toIndexedSeq, label)}).toSeq)
+     new LabelledInstance[Int](features.map(_.toDouble).zipWithIndex.map{case (weight, id) => new WeightedFeature(id,weight)}.toIndexedSeq, label)}).toSeq)
 
 
     val test_data = Source.fromFile(new File("/home/lena/heart_dataset/SPECT.test")).getLines()
 
     val datatest = sc.parallelize(test_data.map(line => {
 
-    val Features = line.split(",").map(_.toDouble).toList
+    val features = line.split(",").map(_.toDouble).toList
 
-        new Instance(Features.zipWithIndex.map{case (weight, id) => new WeightedFeature(id,weight)}.toIndexedSeq)}).toSeq)
+      val feats = features.drop(1)
 
+      new Instance(feats.zipWithIndex.map{case (weight, id) => new WeightedFeature(id,weight)}.toIndexedSeq)}).toSeq)
 
       val train = new BayesEstimator[Int]
-
-      val trainClassifier = new BayesClassifierTrainer[Int](data, train).apply(data) //return classifier
+            println(train)
+      val trainClassifier = new BayesClassifierTrainer[Int](train).apply(data) //return classifier
 
       val classifier = datatest.map(instance => trainClassifier.apply(instance))
 
-      println(classifier)
-  //new ClassifierCrossValidator[Int](0.8,60,new java.util.Random()).apply(new BayesClassifierTrainer[Int](data,train),data)
+      classifier.foreach(println)
+  new ClassifierCrossValidator[Int](0.8,60,new java.util.Random()).apply(new BayesClassifierTrainer[Int](train),data)
 
 
 }
