844cbad2b811b259a08a6cdfb320b99d41fcfcc7
linafru <linafruscianta@gmail.com>
2013-12-11 16:42:40 +0400

    adding elements to map of probabilities

--- src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/AddOneToMap.scala
+++ src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/AddOneToMap.scala
@@ -5,8 +5,6 @@ trait AddOneToMap[K] extends Map[K,Long] {
 
     abstract override def get(key: K): Option[Long] = super.get(key).map(_+1)
 
-    abstract  override def +[B1 >: Long](kv: (K,B1)): Map[K,B1] = super.+(kv)
-
 
 }
 
