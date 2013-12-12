08c342b6eb0b86b9a27417b30ba7ddb66653d8f4
linafru <linafruscianta@gmail.com>
2013-12-12 13:25:01 +0400

    record method get using pattern matching

--- src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/AddOneToMap.scala
+++ src/main/scala/ru/ispras/modis/flint/classifiers/naivebayes/AddOneToMap.scala
@@ -1,25 +1,26 @@
 package ru.ispras.modis.flint.classifiers.naivebayes
 import scala.{Double, Int}
+import scala.collection.immutable.HashMap
 
 
-trait AddOneToMap[K] extends Map[K,Long] {
+class AddOneToMap[K] extends HashMap[K,Long] {
 
-  abstract override def get(key: K): Option[Long] =  {
+  override def get(key: K): Option[Long] =  {
 
-    val x: Option[Long] = super.get(key)
-    x match {
+   super.get(key) match {
+
+     case Some(oldFrequency) => Some(oldFrequency + 1)
 
      case None => Some(1)
-
-     case _ => x.map(_+1)
     }
   }
 
-  abstract override def iterator: Iterator[(K, Long)] = super.iterator
 
-  abstract override def + [B1 >: Long](kv: (K,B1)): Map[K,B1] = super.+(kv)
+  override def iterator: Iterator[(K, Long)] = super.iterator
 
-  abstract override def -(key: K): Map[K,Long] = super.-(key)
+  override def + [B1 >: Long](kv: (K,B1)): Map[K,B1] = super.+(kv)
+
+  override def -(key: K): Map[K,Long] = super.-(key)
 }
 
 
